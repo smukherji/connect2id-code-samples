9680af3b29d7373d5d17b0c89f03649d434a4678
Sayan Mukherji <smukherji@digitalriver.com>
2014-07-04 01:55:47 -0700

    added javadocs

--- src/main/java/com/nimbusds/oauth2/sdk/examples/AuthorizationRequestExample.java
+++ src/main/java/com/nimbusds/oauth2/sdk/examples/AuthorizationRequestExample.java
@@ -1,10 +1,11 @@
 /**
- * 
+ * Copyright (C) 2014. All Rights Reserved.
  */
 package com.nimbusds.oauth2.sdk.examples;
 
 /**
- * @author smukherji
+ * 
+ * @author <a href="mailto:mukherjisayan@gmail.com">Sayan Mukherji</a>
  *
  */
 public class AuthorizationRequestExample {
